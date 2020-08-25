772c67c8bd931cbfb91d65fd4ce35689b972725d
Dasha Serdyuk <d.serdyuk@just-ai.com>
2020-08-25 18:46:36 +0300

    reminder/alarm/timer activities added

--- app/src/main/java/com/justai/aimybox/assistant/AlarmReceiver.kt
+++ app/src/main/java/com/justai/aimybox/assistant/AlarmReceiver.kt
@@ -1,13 +1,19 @@
 package com.justai.aimybox.assistant
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import com.justai.aimybox.components.L
 
 
-
 class AlarmReceiver: BroadcastReceiver() {
+    @SuppressLint("ShowToast")
     override fun onReceive(context: Context?, intent: Intent?) {
         L.d("Alarm!!!!!!")
+        Toast.makeText(context, "whats up bitch", Toast.LENGTH_LONG).show()
     }
+
+
+
 }
